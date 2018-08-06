1c8b5eabe6628ef88e61763637b8db76f9f85f6b
Familia <Familia@DESKTOP-VH683E6>
2018-08-05 21:30:36 -0300

    Criado classe

---
+++ src/br/com/imobiliaria/entities/Pessoa.java
@@ -0,0 +1,17 @@
+package br.com.imobiliaria.entities;
+
+public class Pessoa {
+
+	
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+}
